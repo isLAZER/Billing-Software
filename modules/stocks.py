06b20a63b7fb95828a5417ce796d3777ed095720
kazuha-san <sagarshrivastava2304@gmail.com>
2022-12-01 09:58:02 +0530

    update-2

--- modules/stocks.py
+++ modules/stocks.py
@@ -20,10 +20,32 @@ def shop():
                 print("No Items in shop currently!")
             else:
                 for row in data:
-                    table.rows.append(row)    
-            
+                    table.rows.append(row)        
             print(table)
-
+        
+        #Adding new entry 
+        elif x==2:
+            i_code=input("Enter the Item code : ")
+            i_name=input("Enter the Item name : ")
+            i_brand=input('Enter the Brand name : ')
+            i_price =input("Enter the rate per item: ")
+            i_catg=input('Enter the category of that item: ')
+            i_dis=input("Enter the discount percentage : ")
+            mysql_csr.execute(f"insert into itemshop values('{i_code}','{i_name}','{i_brand}','{i_catg}','{i_price}/-','{i_dis} %')")
+            ms.commit()
+            print("ITEM ADDED TO DATABASE!")
+        
+        #deleting an entry
+        elif x==3:
+            code=input("Enter the code of the item you want to remove from database: ")
+            try:
+                mysql_csr.execute(f"DELETE FROM productInfo WHERE Item_code='{code}';  ")
+                print("Item removed from datbase ")
+                ms.commit()
+            except:
+                print("AN ERROR OCCURRED\nFAILED TO DELETE ENTRY!")
+        
+        #complex updating part 
 
 
 def stocks():
@@ -47,29 +69,17 @@ def stocks():
         #updating stocks
         elif x==2:
             cod=input('Enter the Code of the Item to update stocks of: ')
-            #work in progress  
+            #work in progress 
+            # features
+            # 1. update the amt in stock
+            # 2. update status of stock
+            # 3. profit count
             
-        #
         '''
-        if x==2:
-            item_code=input("Enter the Item code : ")
-            item_name=input("Enter the Item name : ")
-            item_stocks=input('Enter the number of items: ')
-            item_price =input("Enter the rate per item: ")
-            category_item=input('Enter the category of that item: ')
-            mysql_csr.execute(f"insert into productInfo values('{item_code}','{item_name}',{item_stocks},{item_price},'{category_item}');")
-            ms.commit()
-            print("ITEM ADDED TO DATABASE")
-            
+        
         #Below code will REMOVE an item from stock
         if x==3:
-            code=input("Enter the code of the item you want to remove from database: ")
-            try:
-                mysql_csr.execute(f"DELETE FROM productInfo WHERE Item_code='{code}';  ")
-                print("Item removed from datbase ")
-                ms.commit()
-            except:
-                print("SOME ERROR HAPPENED AT OUR END :-(")
+            
         #Below code will increase the number of items in stock
         if x==4:
             code = input("Enter the code of the item of which you want to increase: ")
