ff13aba3318fa17a64cefff06835365d982b89dd
kazuha-san <sagarshrivastava2304@gmail.com>
2022-12-01 12:52:15 +0530

    hmm

--- modules/login.py
+++ modules/login.py
@@ -12,16 +12,16 @@ def login(user,passwd):
     if user==username and passwd==password:
         return "ADMIN"
     else:
-        cred = open("Backend/user_cred.dat","rb")
+        cred = open("Backend/customer.dat","rb")
         data = pickle.load(cred)
         for i in data:
             if i[0] == user and i[1] == passwd:
                 print(f"Welcome {user}!!!")
             else:
                 cred.close()
-                cred = open("Backend/user_cred.dat","wb")
+                cred = open("Backend/customer.dat","wb")
                 pickle.dump(data+[[user,passwd]],cred)
-                print(f"New User Detected. Signup completed as {user}!!!")
+                print(f"New User Detected. Signup completed!")
         return str(user)
 
 credentials.close()
